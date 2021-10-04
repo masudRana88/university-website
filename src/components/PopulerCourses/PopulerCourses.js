3f6ccb11beaad45873271256794b24d6d7f78ec8
masudRana88 <mahimuddin88@gmail.com>
2021-10-04 16:14:30 +0600

    about pages is add

--- src/components/PopulerCourses/PopulerCourses.js
+++ src/components/PopulerCourses/PopulerCourses.js
@@ -9,7 +9,7 @@ const PopulerCourses = () => {
     return (
         <div className="container coursess-container">
             <h6 className="text-center section-name">Popular Courses</h6>
-            <h1 className="text-center section-title">Lets Brows All Catagori</h1>
+            <h1 className="text-center section-title">Lets Browes All Catagory</h1>
             <div className="row row-cols-1 row-cols-md-2 g-4">
                 {
                     populerCources.map(course =><div key={course.id} className="col">
@@ -18,7 +18,7 @@ const PopulerCourses = () => {
                             <div className="card-body">
                                 <h5 className="card-title">{course.title}</h5>
                                 <p className="card-text">{course.description.slice(0, 150)}...</p>
-                                <Link to="home">
+                                <Link to="/courses">
                                     <button type="button" className="get-started-button">Read More<i className="bi bi-chevron-compact-right"></i></button>
                                 </Link>
                             </div>
--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './About.css'
 const About = () => {
     return (
-        <div>
-           this is About 
+        <div className="about-us container">
+            <div>
+                <h1 className="setcion-name text-center">About us</h1>
+            </div>
+            <h4>Our Mission</h4>
+            <p>
+                A mission statement clarifies what the company wants to achieve, who they want to support, and why they want to support them. On the other hand, a vision statement describes where the company wants a community, or the world, to be as a result of the company's services. Thus, a mission statement is a roadmap for the company's vision statement
+            </p>
+            <h4>Our Vision</h4>
+            <p>
+                The Life is Good brand is about more than spreading optimism — although, with uplifting T-shirt slogans like "Seas The Day" and "Forecast: Mostly Sunny," it's hard not to crack a smile.
+
+                There are tons of T-shirt companies in the world, but Life is Good's mission sets itself apart with a mission statement that goes beyond fun clothing: to spread the power of optimism.
+            </p>
         </div>
     );
 };
